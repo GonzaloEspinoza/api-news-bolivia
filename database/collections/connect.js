95d279633350c90c75de47c425c9c65a4e56fc41
GonzaloEspinoza <gonis1990@gmail.com>
2020-05-15 11:59:49 -0400

    mongodb atlas

--- database/collections/connect.js
+++ database/collections/connect.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
+// mongo atlas:  mongodb+srv://gonzalo3:<password>@cluster0-dza4n.mongodb.net/test?retryWrites=true&w=majority
+
 try {
-    mongoose.connect('mongodb://192.168.99.100:27017/newsDB',{useNewUrlParser: true, useUnifiedTopology:true})
+    mongoose.connect('mongodb+srv://gonzalo3:newspasswordmongodb@cluster0-dza4n.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology:true})
    
    .then(()=>{
     console.log('conected database mongodb ok')
